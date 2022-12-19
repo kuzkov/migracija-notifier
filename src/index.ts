1eb5d5873cc9a211bd4c6afa1725f1cb51edfaf1
Artyom Kuzkov <artyom.kuzkov@yandex.ru>
2022-12-19 23:48:30 +0200

    fix: making typescript work with telegraf

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,53 @@
-import { Context, Markup, Telegraf } from "telegraf";
-import { Update } from "typegram";
+import { Context, Markup, Scenes, Telegraf } from "telegraf";
 import { TELEGRAF_BOT_TOKEN } from "./env";
+import LocalSession from "telegraf-session-local";
 
 // @ts-ignore
 import Calendar from "telegraf-calendar-telegram";
 
-const bot: Telegraf<Context<Update>> = new Telegraf(
+const bot = new Telegraf<SceneContextMessageUpdate>(
   TELEGRAF_BOT_TOKEN as string
 );
+const localSession = new LocalSession({ database: "data/local.json" });
 
-const calendar = new Calendar(bot);
+bot.use(localSession.middleware());
+
+const baseScene = new Scenes.BaseScene("base_scene");
+
+baseScene.enter((context) => {
+  context.reply("Base scene");
+});
+
+const stage = new Scenes.Stage([baseScene]);
+
+bot.use(stage.middleware());
 
 // Start bot
-bot.start((ctx) => {
-  ctx.reply("Hello " + ctx.from.first_name + "!");
+bot.start((context) => {
+  context.scene.enter("base_scene");
 });
 
-// Start tracking
-bot.command("start_tracking", (context) =>
-  context.reply(
-    "Введите по какое число включительно вы хотите искать свободные слоты",
-    calendar.getCalendar()
-  )
-);
+// // Start tracking
+// bot.command("start_tracking", (context) =>
+//   context.reply(
+//     "Введите по какое число включительно вы хотите искать свободные слоты",
+//     calendar.getCalendar()
+//   )
+// );
 
-calendar.setDateListener((context: Context, date: string) => {
-  context.reply(date);
-});
+// calendar.setDateListener((context: Context, date: string) => {
+//   context.reply(date);
+// });
 
-// Help
-bot.help((ctx) => {
-  ctx.reply(
-    "Send /start to receive a greeting\n" +
-      "Send /start_tracking to start tracking available slots\n" +
-      "Send /stop_tracking to start tracking available slots\n" +
-      "Send /quit to stop the bot"
-  );
-});
+// // Help
+// bot.help((ctx) => {
+//   ctx.reply(
+//     "Send /start to receive a greeting\n" +
+//       "Send /start_tracking to start tracking available slots\n" +
+//       "Send /stop_tracking to start tracking available slots\n" +
+//       "Send /quit to stop the bot"
+//   );
+// });
 
 bot.command("quit", (ctx) => {
   ctx.telegram.leaveChat(ctx.message.chat.id);
